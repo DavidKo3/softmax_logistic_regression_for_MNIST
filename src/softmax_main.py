f9a574585592301e375b5eb7ad9ca890633446f3
tensor <tensor@tensor-VirtualBox>
2016-11-03 11:55:30 +0900

    ..

--- src/softmax_main.py
+++ src/softmax_main.py
@@ -18,3 +18,21 @@ b = tf.Variable(tf.zeros([10]))
 
 # Our hypothesis
 activation = tf.add(tf.matmul(X, W),b)  # Softmax
+
+# Cost function: cross entropy
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(activation, Y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost) # Gradient Descent
+
+# Before starting, initialize the variables. We will 'run' this first.
+init = tf.initialize_all_variables()
+
+# Launch the graph
+with tf.Session() as sess:
+    sess.run(init)
+
+
+
+
+
+
+
